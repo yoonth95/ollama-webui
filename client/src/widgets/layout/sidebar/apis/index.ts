1e379e998a823458157dafb731a77b781ada8fbe
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-25 21:49:19 +0900

    Delete: Next 코드 옮기는 과정에서 사용하지 않는 파일 삭제

--- client/src/widgets/layout/sidebar/apis/index.ts
+++
@@ -1,5 +0,0 @@
-import getChatRooms from "./getChatRooms";
-import deleteChatRoom from "./deleteChatRoom";
-import updateChatRoomTitle from "./updateChatRoomTitle";
-
-export { getChatRooms, deleteChatRoom, updateChatRoomTitle };
