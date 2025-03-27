0185b2be1f02e9dfbb2ff961fdde91931c1177c5
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-27 13:07:57 +0900

    Feat: hr 규칙 추가 (최상단에서 p태그 추가)

--- client/src/features/chatEditor/utils/tiptapExtensionUtil.ts
+++ client/src/features/chatEditor/utils/tiptapExtensionUtil.ts
@@ -1,4 +1,19 @@
-import { Extension } from "@tiptap/core";
+import { Editor, Extension } from "@tiptap/core";
+
+// 빈 문단 추가
+const insertParagraphAtStart = (editor: Editor) => {
+  editor.commands.insertContentAt(0, { type: "paragraph", content: [] });
+  editor.commands.focus("start");
+  return true;
+};
+
+type ResolvedPos = {
+  depth: number;
+  index: (depth: number) => number;
+};
+
+// 첫 번째 노드인지 확인인
+const isFirstNode = ($from: ResolvedPos) => $from.depth === 1 && $from.index($from.depth - 1) === 0;
 
 const ShiftEnterExtension = Extension.create({
   name: "shiftEnter",
@@ -32,10 +47,8 @@ const CodeBlockEnhancementExtension = Extension.create({
         const isAtStart = from === $from.start();
 
         // 코드 블록의 맨 앞부분이고, 이 코드 블록이 문서의 첫 번째 노드인 경우
-        if (isInCodeBlock && isAtStart && $from.depth === 1 && $from.index($from.depth - 1) === 0) {
-          editor.commands.insertContentAt(0, { type: "paragraph", content: [] }); // 빈 문단 추가 (placeholder 비활성화)
-          editor.commands.focus("start"); // 새로 추가된 문단으로 커서 이동
-          return true;
+        if (isInCodeBlock && isAtStart && isFirstNode($from)) {
+          return insertParagraphAtStart(editor);
         }
 
         return false;
@@ -44,4 +57,25 @@ const CodeBlockEnhancementExtension = Extension.create({
   },
 });
 
-export { ShiftEnterExtension, CodeBlockEnhancementExtension };
+const HorizontalRuleEnhancementExtension = Extension.create({
+  name: "horizontalRuleEnhancement",
+  addKeyboardShortcuts() {
+    return {
+      ArrowUp: ({ editor }) => {
+        // 현재 선택 위치 확인
+        const { selection } = editor.state;
+        const { $from } = selection;
+
+        // HR 요소가 제일 위에 있는 경우 paragraph 추가
+        const isAtHr = $from.node(-1)?.firstChild?.type.name === "horizontalRule";
+        if (isAtHr) {
+          return insertParagraphAtStart(editor);
+        }
+
+        return false;
+      },
+    };
+  },
+});
+
+export { ShiftEnterExtension, CodeBlockEnhancementExtension, HorizontalRuleEnhancementExtension };
