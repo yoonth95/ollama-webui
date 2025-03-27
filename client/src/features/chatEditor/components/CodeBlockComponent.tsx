761215ad65689b39ce2029e105e93153a341b6bc
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-27 13:08:58 +0900

    Refactor: CodeBlockHighlightJS 삭제 및 기존 코드로 변경경

--- client/src/features/chatEditor/components/CodeBlockComponent.tsx
+++ client/src/features/chatEditor/components/CodeBlockComponent.tsx
@@ -1,27 +1,10 @@
-import { NodeViewContent, NodeViewWrapper, NodeViewProps } from "@tiptap/react";
-import { useEffect, useRef } from "react";
-import highlight from "highlight.js";
+import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
 
-interface CodeBlockProps extends NodeViewProps {
-  updateAttributes: (attributes: Partial<{ language: "auto" | string }>) => void;
-}
-
-const CodeBlockComponent = ({ node }: CodeBlockProps) => {
-  const codeRef = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    if (codeRef.current) {
-      // highlight.js로 구문 강조 적용
-      highlight.highlightElement(codeRef.current);
-    }
-  }, [node.textContent]); // 텍스트가 변경될 때마다 강조 적용
-
+const CodeBlockComponent = () => {
   return (
     <NodeViewWrapper className="code-block">
       <pre>
-        <code ref={codeRef} data-language={node.attrs.language}>
-          <NodeViewContent as="span" />
-        </code>
+        <NodeViewContent as="code" />
       </pre>
     </NodeViewWrapper>
   );
