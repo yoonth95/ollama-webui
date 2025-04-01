d821993c515dac3a52c5544d420537afb571aa15
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-04-02 00:29:17 +0900

    Refactor: EditorContainer에서 useRef 제거 및 ChatEditorWrapper에 editorRef 전달 제거

--- client/src/features/chatEditor/EditorContainer.tsx
+++ client/src/features/chatEditor/EditorContainer.tsx
@@ -1,14 +1,10 @@
-import { useRef } from "react";
 import { ChatEditorWrapper } from "@/features/chatEditor/components";
-import { TiptapEditorRef } from "@/features/chatEditor/types/TiptapEditorType";
 import "@/features/chatEditor/styles/editor.css";
 
 const EditorContainer = () => {
-  const editorRef = useRef<TiptapEditorRef | null>(null);
-
   return (
-    <section className="mx-auto mb-8 flex w-full gap-4 text-base md:max-w-[40rem] md:gap-5 lg:gap-6 xl:max-w-[48rem]">
-      <ChatEditorWrapper editorRef={editorRef} />
+    <section className="mx-auto mb-8 flex w-full gap-4 text-base md:max-w-[44rem] md:gap-5 lg:gap-6 xl:max-w-[48rem]">
+      <ChatEditorWrapper />
     </section>
   );
 };
