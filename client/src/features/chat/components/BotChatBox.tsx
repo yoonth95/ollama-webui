fe9a8104f4f64b3a396a54d9f0a46523d50ab8ec
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-04-06 00:58:43 +0900

    Refactor: 마크다운 렌더러를 마크다운 뷰어로 변경 및 스타일 수정

--- client/src/features/chat/components/BotChatBox.tsx
+++ client/src/features/chat/components/BotChatBox.tsx
@@ -1,13 +1,13 @@
-import MarkdownRenderer from "@/features/markdownRender/MarkdownRenderer";
+import MarkdownViewer from "@/features/markdown/MarkdownViewer";
 
 const BotChatBox = ({ content }: { content: string }) => {
   return (
     <article className="w-full">
       <div className="mx-auto my-auto py-[18px] text-base">
-        <div className="mx-auto flex w-full flex-1 justify-start gap-4 text-base md:max-w-[40rem] md:gap-5 lg:gap-6 xl:max-w-[48rem]">
-          <div className="max-w-full">
+        <div className="mx-auto flex w-full flex-1 justify-start gap-4 text-base md:max-w-[40rem] md:gap-5 lg:gap-6 xl:max-w-[46rem]">
+          <div className="max-w-[100%] px-3">
             <div className="rounded-2xl px-4 py-2 break-words dark:bg-transparent">
-              <MarkdownRenderer content={content} type="Bot" />
+              <MarkdownViewer content={content} />
             </div>
           </div>
         </div>
--- client/src/features/chat/components/UserChatBox.tsx
+++ client/src/features/chat/components/UserChatBox.tsx
@@ -1,13 +1,13 @@
-import MarkdownRenderer from "@/features/markdownRender/MarkdownRenderer";
+import MarkdownViewer from "@/features/markdown/MarkdownViewer";
 
 const UserChatBox = ({ content }: { content: string }) => {
   return (
-    <article className="w-full">
-      <div className="mx-auto my-auto px-6 py-[18px] text-base">
-        <div className="mx-auto flex w-full flex-1 justify-end gap-4 text-base md:max-w-[40rem] md:gap-5 lg:gap-6 xl:max-w-[48rem]">
-          <div className="max-w-[75%] px-3">
-            <div className="dark:bg-accent bg-accent rounded-2xl px-5 py-2 break-words">
-              <MarkdownRenderer content={content} type="User" />
+    <article className="w-full overflow-hidden">
+      <div className="mx-auto my-auto overflow-hidden px-6 py-[18px] text-base">
+        <div className="mx-auto flex w-full flex-1 justify-end gap-4 overflow-hidden text-base md:max-w-[40rem] md:gap-5 lg:gap-6 xl:max-w-[46rem]">
+          <div className="max-w-[100%] overflow-hidden px-3 sm:max-w-[80%]">
+            <div className="dark:bg-accent bg-accent overflow-hidden rounded-2xl px-5 py-2 break-words">
+              <MarkdownViewer content={content} />
             </div>
           </div>
         </div>
