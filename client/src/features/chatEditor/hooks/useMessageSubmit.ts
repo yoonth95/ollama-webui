3c9e519c58c08e61d4dad1b6456e8f57d57c9ed6
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-04-08 00:47:50 +0900

    Feat: 메시지 제출 시 이미지와 텍스트 입력 검증 추가 및 성공 시 이미지 스토어 삭제

--- client/src/features/chatEditor/hooks/useMessageSubmit.ts
+++ client/src/features/chatEditor/hooks/useMessageSubmit.ts
@@ -1,9 +1,9 @@
 import { useModelSelectStore } from "@/shared/stores/useModelSelectStore";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { TiptapEditorRef } from "@/features/chatEditor/types/TiptapEditorType";
 import useCreateChatRoom from "@/features/chatEditor/queries/useCreateChatRoom";
 import { useEditorImageStore } from "@/features/chatEditor/stores/EditorImageStore";
+import { TiptapEditorRef } from "@/features/chatEditor/types/TiptapEditorType";
 
 const TOAST_ID = "model-select-toast";
 
@@ -12,10 +12,9 @@ export const useMessageSubmit = (editorRef: React.RefObject<TiptapEditorRef | nu
   const { chatRoomId } = useParams<{ chatRoomId?: string }>();
 
   const selectedModel = useModelSelectStore((state) => state.selectedModel);
-  const getImages = useEditorImageStore((state) => state.getImages);
+  const { getImages, clearImages } = useEditorImageStore();
 
   const { mutate: createChatRoom, isPending: isCreatingRoom } = useCreateChatRoom();
-  // const { mutate: sendMessage, isPending: isSendingMessage } = useSendMessage();
 
   const handleSubmit = async () => {
     if (isCreatingRoom) return;
@@ -26,20 +25,31 @@ export const useMessageSubmit = (editorRef: React.RefObject<TiptapEditorRef | nu
       }
       return;
     }
-    const content = editorRef?.current?.getText();
-    if (!content) return;
 
+    // 이미지 또는 텍스트 중 하나만 있으면 됨
+    const content = editorRef?.current?.getText() || "";
     const images = await getImages();
+
+    if (images.length === 0 && !content) {
+      if (!toast.isActive(TOAST_ID)) {
+        toast.error("메시지를 입력해주세요.", { toastId: TOAST_ID });
+      }
+      return;
+    }
 
     const body =
       images.length > 0 ? { model: selectedModel.model, content, images } : { model: selectedModel.model, content };
 
     if (!chatRoomId) {
       createChatRoom(body, {
-        onSuccess: (data) => navigate(`/chat/${data.data?.id}`),
+        onSuccess: (data) => {
+          navigate(`/chat/${data.data?.id}`);
+          clearImages();
+        },
       });
     } else {
       console.log("채팅방에서 메시지 입력");
+      clearImages();
     }
   };
 
