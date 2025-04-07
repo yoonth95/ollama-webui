9af54d2b8b66fa6964ad21d2bc4e89859c9b10dc
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-04-07 17:01:33 +0900

    Refactor: 채팅 박스 컴포넌트 스타일 수정 및 구조 개선

--- client/src/features/chat/components/BotChatBox.tsx
+++ client/src/features/chat/components/BotChatBox.tsx
@@ -2,14 +2,10 @@ import MarkdownViewer from "@/features/markdown/MarkdownViewer";
 
 const BotChatBox = ({ content }: { content: string }) => {
   return (
-    <article className="w-full">
-      <div className="mx-auto my-auto py-[18px] text-base">
-        <div className="mx-auto flex w-full flex-1 justify-start gap-4 text-base md:max-w-[40rem] md:gap-5 lg:gap-6 xl:max-w-[46rem]">
-          <div className="max-w-[100%] px-3">
-            <div className="rounded-2xl px-4 py-2 break-words dark:bg-transparent">
-              <MarkdownViewer content={content} />
-            </div>
-          </div>
+    <article className="bot-message flex w-full justify-start">
+      <div className="max-w-[100%] px-3">
+        <div className="px-5 py-2 break-words">
+          <MarkdownViewer content={content} />
         </div>
       </div>
     </article>
--- client/src/features/chat/components/UserChatBox.tsx
+++ client/src/features/chat/components/UserChatBox.tsx
@@ -2,14 +2,10 @@ import MarkdownViewer from "@/features/markdown/MarkdownViewer";
 
 const UserChatBox = ({ content }: { content: string }) => {
   return (
-    <article className="w-full overflow-hidden">
-      <div className="mx-auto my-auto overflow-hidden px-6 py-[18px] text-base">
-        <div className="mx-auto flex w-full flex-1 justify-end gap-4 overflow-hidden text-base md:max-w-[40rem] md:gap-5 lg:gap-6 xl:max-w-[46rem]">
-          <div className="max-w-[100%] overflow-hidden px-3 sm:max-w-[80%]">
-            <div className="dark:bg-accent bg-accent overflow-hidden rounded-2xl px-5 py-2 break-words">
-              <MarkdownViewer content={content} />
-            </div>
-          </div>
+    <article className="user-message flex w-full justify-end">
+      <div className="max-w-[100%] px-3 sm:max-w-[90%] md:max-w-[80%]">
+        <div className="dark:bg-accent bg-accent rounded-2xl px-5 py-2 break-words">
+          <MarkdownViewer content={content} />
         </div>
       </div>
     </article>
