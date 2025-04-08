60bf340de2052eca136e6a9f8a970318b123ebbf
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-04-08 23:51:27 +0900

    Feat: 채팅 컴포넌트에 ChatMessageSkeleton 및 ErrorChatPage 추가

--- client/src/features/chat/components/index.ts
+++ client/src/features/chat/components/index.ts
@@ -1,4 +1,6 @@
 import UserChatBox from "./UserChatBox";
 import BotChatBox from "./BotChatBox";
+import ChatMessageSkeleton from "./ChatMessageSkeleton";
+import ErrorChatPage from "./ErrorChatPage";
 
-export { UserChatBox, BotChatBox };
+export { UserChatBox, BotChatBox, ChatMessageSkeleton, ErrorChatPage };
