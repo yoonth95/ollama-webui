edbb1351a2a5061d4745b6886e2a8448dad172ca
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-04-09 21:31:53 +0900

    Design: ChatMessageSkeleton의 스타일에서 오른쪽 하단 모서리 둥글게 변경

--- client/src/features/chat/components/ChatMessageSkeleton.tsx
+++ client/src/features/chat/components/ChatMessageSkeleton.tsx
@@ -5,7 +5,7 @@ const ChatMessageSkeleton = () => {
     <div className="mb-8 flex items-center justify-center">
       <div className="flex w-full flex-col gap-5 px-3 text-base md:w-[42rem] xl:w-[48rem]">
         <div className="flex flex-col items-end">
-          <Skeleton className="h-20 w-[100%] rounded-3xl rounded-br-sm px-5 py-2 sm:w-[90%] md:w-[80%]" />
+          <Skeleton className="h-20 w-[100%] rounded-3xl rounded-tr-sm px-5 py-2 sm:w-[90%] md:w-[80%]" />
         </div>
         <div className="flex flex-col gap-2">
           <Skeleton className="h-4 w-[90%] rounded-none" />
