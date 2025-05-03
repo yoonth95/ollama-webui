6b0041393cc22e5cd5b3376ea54193aa5798aeeb
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-05-04 00:36:59 +0900

    Design: 사이드바 스켈레톤 디자인 수정

--- client/src/widgets/layout/sidebar/components/ChatRoomSkeleton.tsx
+++ client/src/widgets/layout/sidebar/components/ChatRoomSkeleton.tsx
@@ -1,13 +1,14 @@
 import { Skeleton } from "@/shared/ui/skeleton";
 
 const ChatRoomSkeleton = () => (
-  <div className="mb-8">
+  <div className="p-2">
+    <Skeleton className="h-8 w-full rounded-md" />
     <ul className="space-y-1">
-      {Array(3)
+      {Array(5)
         .fill(0)
         .map((_, index) => (
           <li key={index} className="flex items-center justify-between rounded-md p-2">
-            <Skeleton className="h-6 w-full rounded-md" />
+            <Skeleton className="h-5 w-full rounded-md" />
           </li>
         ))}
     </ul>
