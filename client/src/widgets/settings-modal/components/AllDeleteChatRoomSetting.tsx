6e284412dd71fb6dcdc83ee179cf8ebf5b68f99e
윤태현 <yoonth0919@gmail.com>
2025-06-05 21:36:42 +0900

    Refactor: 채팅방 전체 삭제 문구 수정

--- client/src/widgets/settings-modal/components/AllDeleteChatRoomSetting.tsx
+++ client/src/widgets/settings-modal/components/AllDeleteChatRoomSetting.tsx
@@ -26,7 +26,8 @@ const AllDeleteChatRoomSetting = () => {
       type="delete"
       buttonText="모두 삭제"
       title="모든 채팅방을 삭제하시겠습니까?"
-      description="모든 채팅방이 삭제됩니다. 계속하시겠습니까?"
+      description={`보관된 채팅방을 포함해 모든 채팅방이 삭제됩니다. 
+      계속하시겠습니까?`}
       confirmText="삭제"
       confirmColor="bg-red-500 hover:bg-red-600 dark:bg-red-500 dark:hover:bg-red-600"
       onAction={handleDelete}
